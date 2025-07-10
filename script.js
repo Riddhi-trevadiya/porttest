40346e522787f18674eb5300db6a58235ccdf908
Riddhi-trevadiya <128346342+Riddhi-trevadiya@users.noreply.github.com>
2025-07-10 14:05:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Theme toggle
 const toggle = document.getElementById('theme-toggle');
 const body = document.body;
 
@@ -13,4 +13,16 @@ toggle.addEventListener('click', () => {
   } else {
     localStorage.removeItem('theme');
   }
+});
+
+// Intro logic
+const intro = document.getElementById('intro');
+const main = document.getElementById('main-content');
+const btn = document.getElementById('enter-btn');
+const sound = document.getElementById('intro-sound');
+
+btn.addEventListener('click', () => {
+  intro.style.display = 'none';
+  main.style.display = 'block';
+  sound.play();
 });
